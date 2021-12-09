5c9b2a41533e34d338a6067a7eaacf9dd8b327f0
[Gerald] <devergerald@gmail.com>
2021-12-08 19:19:32 -0800

    trying jupiter

--- src/test/java/testOne.java
+++ src/test/java/testOne.java
@@ -1,22 +1,24 @@
 
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
 import org.psnbtech.Game;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class testOne {
 	Game gameTest = new Game();
-	@Before
+	/*@Before
 	public void setUp() throws Exception {
 		//Game gameTest = new Game();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-	}
+	}*/
 
 	@Test
 	public void test() {
