86cfad0f5ba7c1602a984ddd9cba4ddad54a00f5
[Gerald] <devergerald@gmail.com>
2021-12-08 19:32:34 -0800

    reverted some changes

--- src/test/java/testOne.java
+++ src/test/java/testOne.java
@@ -1,13 +1,13 @@
 
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.psnbtech.Game;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
 
 public class testOne {
 	Game gameTest = new Game();
